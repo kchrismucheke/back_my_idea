73f01a0bd4d1f95efc16387519de1791929022e0
Chris Mucheke <kchrismucheke@gmail.com>
2020-04-14 11:30:59 +0300

    polymorphism

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
-require 'sidekiq/web'
+require "sidekiq/web"
 
 Rails.application.routes.draw do
-  resources :projects
-    authenticate :user, lambda { |u| u.admin? } do
-      mount Sidekiq::Web => '/sidekiq'
-    end
+  get "comments/create"
+  resources :projects do
+    resources :comments, module: :projects
+  end
+
+  authenticate :user, lambda { |u| u.admin? } do
+    mount Sidekiq::Web => "/sidekiq"
+  end
 
   devise_for :users
   root to: "projects#index"
--- db/schema.rb
+++ db/schema.rb
@@ -1,18 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 2020_04_09_210748) do
-
-  # These are extensions that must be enabled in order to support this database
+ActiveRecord::Schema.define(version: 2020_04_11_062023) do
   enable_extension "plpgsql"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
@@ -44,6 +30,16 @@ ActiveRecord::Schema.define(version: 2020_04_09_210748) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commentable_type"
+    t.integer "commentable_id"
+    t.bigint "user_id", null: false
+    t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -82,5 +78,6 @@ ActiveRecord::Schema.define(version: 2020_04_09_210748) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "users"
   add_foreign_key "projects", "users"
 end
